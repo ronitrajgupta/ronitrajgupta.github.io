14f3b1f8085bc1833c993739c5420b380cdd7d5d
ronitrajgupta <59310217+ronitrajgupta@users.noreply.github.com>
2019-12-28 18:09:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+var car = prompt("which car do you want?");
+
+ var name = prompt("whats your name??");
+
+ var day = prompt("In how many days would you like to have the car??")
+
+
+ alert(name + " your " + car +" would arrive in " + day + " days");
